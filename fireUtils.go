9d3e5075498536a6e6d27f4f1e6582ccbf53625d
ryota_kondo <aki_056@outlook.jp>
2018-06-10 13:45:40 +0900

    レスポンス生成関数作成

--- fireUtils.go
+++ fireUtils.go
@@ -7,6 +7,9 @@ import (
 	"io/ioutil"
 )
 
+var Tension [2]int
+var ResponseCount int
+
 // m4a音声をローカルに保存
 func SaveReadFile(data []byte)  {
 	var file *os.File
@@ -35,10 +38,7 @@ func ReadWav() []byte{
 func ConvertM4aToWav() (error) {
 	fmt.Println("cnv start")
 
-	out, err := exec.Command("ls", "-la").Output()
-	fmt.Println(string(out))
-
-	err = exec.Command("sh","./enc.sh").Run()
+	err := exec.Command("sh","./enc.sh").Run()
 	if err != nil {
 		fmt.Println(err)
 		return err
@@ -47,12 +47,90 @@ func ConvertM4aToWav() (error) {
 	return nil
 }
 
-func CreateResponse(empathResponse EmpathResponse) MirrorBowlResponse {
-	var res MirrorBowlResponse
 
-		res = MirrorBowlResponse{
-			Suggestion:"森食ってモリモリ",
-			Tention:49,
+func CreateResponse(empathResponse EmpathResponse) MirrorBowlResponse {
+
+	// 統計処理
+	Tension[0] = Tension[1]
+	Tension[1] = empathResponse.Energy
+	ResponseCount += 1
+
+	if ResponseCount > 1 && ResponseCount % 2 == 0 {
+		var resTension = (Tension[0] + Tension[1])/2
+
+		// 評価回数である場合
+
+		// APIエラーがある場合
+		if empathResponse.Error != 0 {
+			return MirrorBowlResponse{
+				Suggestion: "9",
+				Tention:0,
+				Emotion: Emotion{
+					0,
+					0,
+					0,
+					0,
+					0,
+				},
+			}
+			// APIエラーがない場合
+		} else {
+			// init
+			var checkArry = []int{empathResponse.Calm, empathResponse.Anger, empathResponse.Joy, empathResponse.Sorrow }
+			var checkSuggestion [2]string
+			var resSuggestion string
+
+			// Max感情を判定
+			// Max感情を判定
+			var res int
+			for i , v := range checkArry {
+				var checkValue int
+				if checkValue < v {
+					res = i
+					checkValue = v
+				}
+			}
+
+			// 感情分類
+			switch res {
+			case 0:
+				checkSuggestion[0] = "1"
+				checkSuggestion[1] =  "5"
+			case 1:
+				checkSuggestion[0] = "2"
+				checkSuggestion[1] = "6"
+			case 2:
+				checkSuggestion[0] = "3"
+				checkSuggestion[1] = "7"
+			case 3:
+				checkSuggestion[0] = "4"
+				checkSuggestion[1] = "8"
+			}
+
+			switch {
+			case resTension < 20:
+				resSuggestion = checkSuggestion[1]
+			case resTension >= 20:
+				resSuggestion = checkSuggestion[0]
+			}
+
+			return MirrorBowlResponse{
+				Suggestion: resSuggestion,
+				Tention: resTension,
+				Emotion: Emotion{
+					empathResponse.Calm ,
+					empathResponse.Anger ,
+					empathResponse.Joy ,
+					empathResponse.Sorrow ,
+					empathResponse.Energy,
+				},
+			}
+		}
+	} else {
+		// 評価回数じゃない場合
+		return MirrorBowlResponse{
+			Suggestion: "",
+			Tention: empathResponse.Energy,
 			Emotion: Emotion{
 				empathResponse.Calm ,
 				empathResponse.Anger ,
@@ -61,7 +139,5 @@ func CreateResponse(empathResponse EmpathResponse) MirrorBowlResponse {
 				empathResponse.Energy,
 			},
 		}
-
-
-	return res
+	}
 }
